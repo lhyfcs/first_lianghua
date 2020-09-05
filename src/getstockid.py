deae2b9c0896f4f5fb0cc1deaa31ffd312049287
jinfan liu <jinfan.liu@autodesk.com>
2020-09-05 12:40:18 +0800

    start save stock data

--- src/getstockid.py
+++ src/getstockid.py
@@ -8,29 +8,38 @@ pwd = os.getcwd()
 idsCsvPath=os.path.join(os.path.abspath(os.path.dirname(pwd)+os.path.sep+"."), 'data', 'ids.csv')
 
 basePath = '/User/liujinf/'
-today = datetime.datetime.today()
 
-checkday = (today - datetime.timedelta(weeks=4 * 52)).strftime('%Y-%m-%d')
-print('Get day:', checkday);
-start_date = (today - datetime.timedelta(weeks=4 * 52 + 1)).strftime('%Y-%m-%d')
-end_date = (today - datetime.timedelta(weeks=4 * 52 - 1)).strftime('%Y-%m-%d')
+
+def todaydate():
+    today = datetime.datetime.today()
+    searchday = (today - datetime.timedelta(weeks=4 * 52)).strftime('%Y-%m-%d')
+    print('Get day:', searchday)
+    start_date = (today - datetime.timedelta(weeks=4 * 52 + 1)).strftime('%Y-%m-%d')
+    end_date = (today - datetime.timedelta(weeks=4 * 52 - 1)).strftime('%Y-%m-%d')
 
 # 1. use baostock
-lg = bs.login()
-rs = bs.query_trade_dates(start_date=start_date, end_date=end_date)
-while (rs.error_code == '0') & rs.next():
-    day = rs.get_row_data()
-    if day[1]:
-        checkday = day[0]
-        break
-pro = ts.pro_api()
-ts.pro_api('dec07c5bd502e7b1ee2d5a9f105ea04f62552df07b3c8ab6551dc7d0')
-data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date, list_status, market')
-print(len(data))
-d1 = data['name']
-data = data[(data['list_date'] < checkday) & (~data['name'].str.contains('ST'))]
-print(len(data))
-data.to_csv(idsCsvPath)
+    rs = bs.query_trade_dates(start_date=start_date, end_date=end_date)
+    while (rs.error_code == '0') & rs.next():
+        day = rs.get_row_data()
+        if day[1]:
+            searchday = day[0]
+            break
+    return searchday
+
+class GetStockId:
+    def __init__(self):
+        pass
+
+
+    def downloadStockIdByDate(self):
+        checkday = todaydate()
+        pro = ts.pro_api()
+        ts.pro_api('dec07c5bd502e7b1ee2d5a9f105ea04f62552df07b3c8ab6551dc7d0')
+        data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date, list_status, market')
+        print(len(data))
+        data = data[(data['list_date'] < checkday) & (~data['name'].str.contains('ST'))]
+        print(len(data))
+        data.to_csv(idsCsvPath)
 # rs = bs.query_all_stock(checkday)
 # data_list = []
 # while (rs.error_code == '0') & rs.next():
@@ -38,4 +47,4 @@ data.to_csv(idsCsvPath)
 #
 # result = pd.DataFrame(data_list, columns=rs.fields)
 # result.to_csv('./data/ids.csv')
-bs.logout()
+
--- src/updatestocksdata.py
+++ src/updatestocksdata.py
@@ -3,20 +3,39 @@ import numpy as np
 import tushare as ts
 import os
 import baostock as bs
+import getstockid
+
 
 pwd = os.getcwd()
 rootFolder = os.path.join(os.path.abspath(os.path.dirname(pwd)+os.path.sep+"."), 'data')
 idsCsvPath=os.path.join(rootFolder, 'ids.csv')
 
 pdIds = pd.read_csv(idsCsvPath)['ts_code']
-
+lg = bs.login()
 # pro = ts.pro_api()
 # ts.pro_api('dec07c5bd502e7b1ee2d5a9f105ea04f62552df07b3c8ab6551dc7d0')
-
+checkday = getstockid.todaydate()
 for id in pdIds:
-
-#    idData = pro.daily(td_code=id)
-#    idPath = os.path.join(rootFolder, id + '.csv')
-#    idData.to_csv(idPath)
-
+    # idData = ts.get_hist_data(id.split('.')[0])
+    # idPath = os.path.join(rootFolder, id + '.csv')
+    # idData.to_csv(idPath)
+    ids = id.split('.')
+    print(ids[1].lower() + '.' + ids[0])
+    rs = bs.query_history_k_data_plus(ids[1].lower() + '.' + ids[0],
+                                      "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,isST",
+                                      start_date=checkday, end_date='',
+                                      frequency="d", adjustflag="3")
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        data_list.append(rs.get_row_data())
+    data = pd.DataFrame(data_list, columns=rs.fields)
+    data['ma5'] = data['close'].rolling(5).mean()
+    data['ma10'] = data['close'].rolling(5).mean()
+    data['ma20'] = data['close'].rolling(5).mean()
+    data['ma30'] = data['close'].rolling(5).mean()
+    data['ma60'] = data['close'].rolling(5).mean()
+    data['ma250'] = data['close'].rolling(5).mean()
+    data = data.dropna(how='any')
+    data.to_csv(os.path.join(rootFolder, id + '.csv'))
+bs.logout()
 
